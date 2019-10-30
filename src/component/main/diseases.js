2f579a46c09733a899425aa09cc035d72a03fe11
Pradeep <pradeep99909@gmail.com>
2019-10-30 20:41:06 +0530

    added tooltip

--- src/component/main/diseases.js
+++ src/component/main/diseases.js
@@ -1,23 +1,36 @@
 import React from "react";
 import { connect } from "react-redux";
+import ReactTooltip from 'react-tooltip'
+
+const data=require("./details.json")
+
 class Diseases extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  show(e){
+    e.preventDefault()
+    e.target.dataset['tip']=data[e.target.dataset['disease']].detail
+  }
+
   render() {
     return (
       <div className="diseases">
+        
         {this.props.disease !== null
           ? this.props.disease.map((d, key) => (
-              <div className="disease-main" id={key}>
+              <div className="disease-main" key={key}>
                 <div className="disease-top">
                   <div className="disease-name">{d}</div>
-                  <i className="material-icons">info</i>
+                  <i className="material-icons" data-disease={d}  onMouseOver={this.show} data-tip data-for={`global-${key}`} >info</i>
+                  <ReactTooltip type="info" type="dark" place="bottom" multiline={false} id={`global-${key}`} html={true} /> 
                 </div>
                 <div className="disease-bottom" />
               </div>
             ))
           : null}
+               
       </div>
     );
   }
